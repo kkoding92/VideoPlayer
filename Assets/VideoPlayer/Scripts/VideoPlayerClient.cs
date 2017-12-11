1994b3895e16f25e412c3d0f2b5dab3acb5c84c1
hansam2396 <32948186+hansam2396@users.noreply.github.com>
2017-12-11 19:48:26 +0900

    Add VideoPlayer Server, Client

---
+++ Assets/VideoPlayer/Scripts/VideoPlayerClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerClient : CircleVRTransportBase {
+    public VideoPlayerClient(int maxConnection) : base(maxConnection)
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/VideoPlayer/Scripts/VideoPlayerServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerServer : MonoBehaviour, ICircleVRTransportEventHandler
+{
+    public void OnConnect(int hostId, int connectionId, byte error)
+    {
+    }
+
+    public void OnData(int hostId, int connectionId, int channelId, byte[] data, int size, byte error)
+    {
+    }
+
+    public void OnDisConnect(int hostId, int connectionId, byte error)
+    {
+    }
+
+    public void OnManualUpdate()
+    {
+    }
+}
