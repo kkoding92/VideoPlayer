bb6428345c37600787526a9be22f9eb5de00bae5
hansam2396 <32948186+hansam2396@users.noreply.github.com>
2017-12-13 12:03:39 +0900

    Update VideoPlayer Server

--- Assets/VideoPlayer/Scripts/VideoPlayerServer.cs
+++ Assets/VideoPlayer/Scripts/VideoPlayerServer.cs
@@ -2,22 +2,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VideoPlayerServer : MonoBehaviour, ICircleVRTransportEventHandler
 {
+    [SerializeField] private GameObject HostUI;
+    [SerializeField] private Button playBtn;
+    [SerializeField] private Button pauseBtn;
+    [SerializeField] private Button backBtn;
+    [SerializeField] private Button frontBtn;
+
+    private int conID;
+    private bool checkVideo= false;
+
+    private void Start()
+    {
+        playBtn.onClick.AddListener(delegate { MenuButton(playBtn.name); });
+        pauseBtn.onClick.AddListener(delegate { MenuButton(pauseBtn.name); });
+        backBtn.onClick.AddListener(delegate { MenuButton(backBtn.name); });
+        backBtn.onClick.AddListener(delegate { MenuButton(frontBtn.name); });
+        HostUI.SetActive(false);
+    }
+
     public void OnConnect(int hostId, int connectionId, byte error)
     {
+        if (!checkVideo)
+            return;
+
+        CircleVR.SendDataReliable(conID, CircleVR.Instance.ContentName);
     }
 
     public void OnData(int hostId, int connectionId, int channelId, byte[] data, int size, byte error)
     {
+        string msg = CircleVR.Deserialize(data, CircleVRProtocol.REC_BUFFER_SIZE);
+
+        if (msg.Equals("VideoPlayer"))
+        {
+            checkVideo = true;
+            conID = connectionId;
+        }
     }
 
     public void OnDisConnect(int hostId, int connectionId, byte error)
     {
+        if(conID == connectionId)
+        {
+            checkVideo = false;
+            conID = 0;
+        }
     }
 
     public void OnManualUpdate()
     {
+        if (Input.GetKeyDown("1"))
+            HostUI.SetActive(!HostUI.activeSelf);
+    }
+
+    private void MenuButton(string name)
+    {
+        if (name.Equals("PlayBtn"))
+            CircleVR.SendDataReliable(conID, "Play");
+        else if (name.Equals("PauseBtn"))
+            CircleVR.SendDataReliable(conID, "Pause");
+        else if (name.Equals("backBtn"))
+            CircleVR.SendDataReliable(conID, "Back");
+        else
+            CircleVR.SendDataReliable(conID, "Front");
     }
 }
