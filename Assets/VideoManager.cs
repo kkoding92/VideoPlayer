240982e2836e97565c5114dc39d76fc6efb4c28a
hansam2396 <32948186+hansam2396@users.noreply.github.com>
2017-12-13 13:24:49 +0900

    Add VideoManager

---
+++ Assets/VideoManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+
+[Serializable]
+public struct Configuration
+{
+    public string serverIp;
+    public int serverPort;
+
+    public string network;
+}
+
+public class VideoManager : MonoBehaviour {
+
+    private static VideoManager instance = null;
+
+    public static VideoManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<VideoManager>();
+            
+            return instance;
+        }
+    }
+    
+    [SerializeField] private GameObject cam;
+    [SerializeField] private GameObject display;
+    [SerializeField] private VideoClip[] clip;
+
+    private Configuration config;
+    private CircleVRTransportBase protocol;
+
+    public GameObject Cam
+    {
+        get
+        {
+            return cam;
+        }
+
+        set
+        {
+            cam = value;
+        }
+    }
+
+    public GameObject Display
+    {
+        get
+        {
+            return display;
+        }
+
+        set
+        {
+            display = value;
+        }
+    }
+
+    public VideoClip[] Clip
+    {
+        get
+        {
+            return clip;
+        }
+
+        set
+        {
+            clip = value;
+        }
+    }
+
+    public CircleVRTransportBase Protocol
+    {
+        get
+        {
+            return protocol;
+        }
+
+        set
+        {
+            protocol = value;
+        }
+    }
+
+    private void Start()
+    {
+        LoadConfigure();
+
+        Protocol = new VideoPlayerClient();
+        Protocol.Init(config);
+    }
+
+    private void Update()
+    {
+        if (Protocol == null)
+            return;
+
+        Protocol.ManualUpdate();
+    }
+
+    private void LoadConfigure()
+    {
+#if UNITY_EDITOR
+        config = JsonUtility.FromJson<Configuration>(File.ReadAllText(Application.streamingAssetsPath + "/VideoPlayer.json"));
+#endif
+#if !UNITY_EDITOR && UNITY_STANDALONE
+        config = JsonUtility.FromJson<Configuration>(File.ReadAllText(Application.dataPath + "/../VideoPlayer.json"));
+#endif
+    }
+}
--- Assets/VideoPlayer/Scripts/VideoPlayerClient.cs
+++ Assets/VideoPlayer/Scripts/VideoPlayerClient.cs
@@ -7,13 +7,28 @@ public class VideoPlayerClient : CircleVRTransportBase {
     {
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void ManualUpdate()
+    {
+        base.ManualUpdate();
+    }
+
+    protected override void OnBroadcast(int hostId, byte[] data, int size, byte error)
+    {
+        base.OnBroadcast(hostId, data, size, error);
+    }
+
+    protected override void OnConnect(int hostId, int connectionId, byte error)
+    {
+        base.OnConnect(hostId, connectionId, error);
+    }
+
+    protected override void OnData(int hostId, int connectionId, int channelId, byte[] data, int size, byte error)
+    {
+        base.OnData(hostId, connectionId, channelId, data, size, error);
+    }
+
+    protected override void OnDisconnect(int hostId, int connectionId, byte error)
+    {
+        base.OnDisconnect(hostId, connectionId, error);
+    }
 }
